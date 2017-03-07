a9b252d0b13a4e7a8bf137652225cf975d15a8bb
DonQ <qq535868469@gmail.com>
2017-03-08 00:03:19 +0800

    日常更新

--- Vshow_Ads/Vshow_All_calwer.py
+++ Vshow_Ads/Vshow_All_calwer.py
@@ -13,9 +13,21 @@ import requests
 def get_img(video_data):
     global error_times
     try:
-        image = s.get(video_data['img_url']).content
+        image = s.get(video_data['img_url'], timeout=5).content
         with open('vshow_pic/{v_id}.jpg'.format(v_id=video_data['v_id']), 'wb') as fp:
             fp.write(image)
+        error_times = 0
+    except requests.exceptions.ReadTimeout, e:
+        print 'requests.exceptions.ReadTimeout', e
+        time.sleep(5)
+        error_times += 1
+        if error_times == 4:
+            with open('error_log.txt', 'w') as f:
+                f.write(video_data['v_id'])
+                f.write('\r\n')
+            error_times = 0
+        else:
+            get_img(video_data)
     except requests.exceptions.ConnectionError, e:
         print 'requests.exceptions.ConnectionError', e
         time.sleep(5)
@@ -44,7 +56,7 @@ for url in url_list:
         get_img(i)
         print "Done~"
     df = pd.DataFrame(data)
-    df.to_csv('vshow_data/vshow_{index}.csv'.format(index=index))
+    df.to_csv('vshow_data/vshow_{index}.csv'.format(index=index), index=False, encoding='utf-8')
     index += 1
     # print df
     # break
