0c21c6c8ed8a329ce592d508bf4d331835942bc9
K580S_Don <qq535868469@gmail.com>
2017-04-14 00:47:04 +0800

    2017.04.14 在家更新了Vshow去重的桌面路径

--- Vshow_Ads/Drop_duplicates_data.py
+++ Vshow_Ads/Drop_duplicates_data.py
@@ -4,8 +4,18 @@
 __author__ = "DonQ"
 
 import pandas as pd
+import _winreg
+import os
+
+
+def get_desktop():
+    key = _winreg.OpenKey(_winreg.HKEY_CURRENT_USER,
+                          r'Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders')
+    return _winreg.QueryValueEx(key, "Desktop")[0]
+
 
 df = pd.read_csv('vshow_data/vshow_data.csv')
 df = df.drop_duplicates('v_id')
 print df
-df.to_csv('C:/Users/donq2/Desktop/vshow_DropDuplicates.csv', index=False, encoding='utf-8')
+
+df.to_csv(os.path.join(get_desktop(), 'vshow_DropDuplicates.csv'), index=False, encoding='utf-8')
