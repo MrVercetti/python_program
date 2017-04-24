86820a7ced3e7e25083c8949f5647e5b09d6eb94
DonQ <qq535868469@gmail.com>
2017-04-24 20:00:23 +0800

    2017.04.24 在公司又更新了 “成效”>“移动应用安装”

--- Aliexpress_analysis/Analysis_Lily.py
+++ Aliexpress_analysis/Analysis_Lily.py
@@ -26,6 +26,6 @@ for i in df.index:
     df.loc[i, u'os'] = df.loc[i, u'广告系列名称'].split('_')[1]
     df.loc[i, u'country'] = df.loc[i, u'广告系列名称'].split('_')[2]
 
-df[u'margin'] = df[u'margin_cpa'] * df[u'成效 [点击后 1 天]']
+df[u'margin'] = df[u'margin_cpa'] * df[u'移动应用安装 [点击后 1 天]']
 print df
 df.to_csv(os.path.join(get_desktop(), 'Analysis_Lily.csv'), index=False, encoding='gbk')
