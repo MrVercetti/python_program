e1ae9cb6e4c70c8524e588b64f6819116b02e925
K580S_Don <qq535868469@gmail.com>
2017-05-26 19:27:11 +0800

    2017.05.26 在公司远程更新了adsee爬虫，结果账号上限300，未果。

--- AdSeedata_Crawler/parse_adsee.py
+++ AdSeedata_Crawler/parse_adsee.py
@@ -7,11 +7,12 @@ from bs4 import BeautifulSoup
 
 with open('adsee.html', 'r') as fp:
     soup = BeautifulSoup(fp.read(),'lxml')
-    for i in soup.select('#ads > div > div.right_a_d_l > div.rank_right_a_img_div > div > div > img'):
-        print i
+    for i in soup.select('#ads > div > div.right_a_d_l > div.rank_right_a_img_div > div > video > source'):
+        print i.attrs['src']
 
 """
 #ads > div:nth-child(1) > div.right_a_d_l > p.right_a_d_l_text
 #ads > div:nth-child(1) > div.right_a_d_l > div.rank_right_a_img_div > div > div > img
 #ads > div:nth-child(720) > div.right_a_d_l > p.right_a_d_l_text
+#ads > div:nth-child(1) > div.right_a_d_l > div.rank_right_a_img_div > div > video
 """
