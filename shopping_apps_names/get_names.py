35244eaf506d67a38f19d66fae85a6e8d194a292
DonQ <qq535868469@gmail.com>
2017-06-15 16:54:23 +0800

    2017.6.15 在公司更新了Appannie竞品词的selector

--- shopping_apps_names/get_names.py
+++ shopping_apps_names/get_names.py
@@ -11,8 +11,11 @@ from furl import furl
 with open('hehe.html', 'r') as web_data:
     soup = BeautifulSoup(web_data, 'lxml')
     app_names = soup.select(
-        '#aa-app > div > div > div > div:nth-of-type(2) > app-group-table-container > div > div.ng-isolate-scope > div > div.dashboard-table.fixed-columns-table-container > div:nth-of-type(3) > table > tbody > tr > td.app-v2.tbl-col-app-v2.ng-scope > div > div > div.main-info > div > div.app-link-container > a > span')
+        '#sub-container > div.main.storestats_wrapper.page_top > div.inner > div.frame.frame-ss > div > div > div > div:nth-of-type(1) > div:nth-of-type(2) > app-group-table-container > div > div.ng-isolate-scope > div > div.dashboard-table.fixed-columns-table-container > div:nth-of-type(3) > table > tbody > tr > td.app-v2.tbl-col-app-v2.ng-scope > div > div > div.main-info > div > div.app-link-container > a > span'
+    )
+    # print app_names
     with open('names.txt', 'w') as f:
+
         for i in app_names:
             name = i.get_text().split('-')[0].encode('utf-8')
             f.write(name)
@@ -20,6 +23,5 @@ with open('hehe.html', 'r') as web_data:
             print name
 
 # os.remove('hehe.html')
-
 
 
